d01e637e9e848ebabd8e97542bba794941f1317d
Andrei Marin <hedrox53@gmail.com>
2016-11-02 22:21:11 +0200

    added keras model

--- run.py
+++ run.py
@@ -1,3 +1,6 @@
+from keras.datasets import Convolution1D
+from keras.models import Sequential
+from keras.layers import Activation, Dense, Flatten
 import os
 import csv
 from collections import defaultdict
@@ -14,3 +17,21 @@ for record in dataset_files:
         reader.next()
         for row in reader:
             data[record].append(float(row[1]))
+
+nb_filters = 32
+
+model = Sequential()
+model.add(Convolution1D(nb_filters, 3, input_shape=(500,), activation='relu'))
+model.add(Dropout(0.25))
+model.add(Convolution1D(nb_filters, 3, activation='relu'))
+model.add(Dropout(0.25))
+model.add(Convolution1D(nb_filters, 3, activation='relu'))
+model.add(Dropout(0.25))
+
+
+model.add(Flatten())
+model.add(Dense(256, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(256, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(2), activation='softmax')
